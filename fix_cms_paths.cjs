8dd237f09ab8b88d8425d848830579c8e8246887
robinfrancis186 <robinfrancis186@gmail.com>
2026-02-24 21:44:52 +0530

    fix(cms): patch module import map paths to prevent 404 on live site

--- fix_cms_paths.cjs
+++ fix_cms_paths.cjs
@@ -6,6 +6,8 @@ if (fs.existsSync(indexPath)) {
     let content = fs.readFileSync(indexPath, 'utf-8');
     content = content.replace(/href="\/static/g, 'href="/cms/static');
     content = content.replace(/src="\/static/g, 'src="/cms/static');
+    // Also patch the import map JSON paths
+    content = content.replace(/"\/vendor\//g, '"/cms/vendor/');
     fs.writeFileSync(indexPath, content);
     console.log('Successfully patched CMS absolute asset paths.');
 } else {
