19d7bd4e85062be99bac3d0192919814c3a36cc2
robinfrancis186 <robinfrancis186@gmail.com>
2026-03-12 15:35:37 +0530

    fix: restore 3D carousel to homepage and integrate with CMS

--- docs/assets/Projects-BDfiBEJE.js
+++ docs/assets/Projects-BDfiBEJE.js
@@ -1,4 +1,4 @@
-import{c as Zn,r as Y,j as A,a as Ae,m as $t}from"./index-D3NOx8QV.js";/**
+import{c as Zn,r as Y,j as A,a as Ae,m as $t}from"./index-jEW73j8k.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
